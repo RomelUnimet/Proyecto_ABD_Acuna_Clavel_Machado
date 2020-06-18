7f7c07fb6372481a4911dff92f46e43f70488eba
WilCloud <wilfredo.machado@correo.unimet.edu.ve>
2020-06-18 14:34:28 -0400

    param

--- stored_procedures.py
+++ stored_procedures.py
@@ -13,10 +13,14 @@ conn = psycopg2.connect(
 #Para este procedimiento se crea un nuevo type en la base de datos.
 def createCart():
     query = """ CREATE TYPE cart AS (product VARCHAR(30), qty INTEGER); """
+    cur = conn.cursor()
+    cur.execute(query)
+    cur.close()
+    conn.commit()
 
 
 #Luego se realiza la función de compra
-def buyProcedure():
+def buyProcedure(product, bank, ci_client, date_transaction, store):
 
     query=""" CREATE OR REPLACE FUNCTION shop (product cart[], bank varchar(15), ci_client varchar(20), date_transaction timestamp,
     store integer)
@@ -41,14 +45,19 @@ def buyProcedure():
     cur.execute(query)
     cur.close()
     conn.commit()
-    
+
+    query = f""" SELECT shop({product}, {bank}, {ci_client}, {date_transaction}, {store})"""
+    cur = conn.cursor()
+    cur.execute(query)
+    cur.close()
+    conn.commit()
     
 
 
 
 #Procedimiento 2:
 
-def account_state():
+def account_state(curr_day):
     query=""" CREATE OR REPLACE FUNCTION account_state (curr_day TIMESTAMP)
     RETURNS TABLE (bank varchar(15), balance numeric(32,2), date_meassured date ) AS $$
     DECLARE
@@ -82,7 +91,7 @@ def account_state():
     conn.commit()
 
     
-    query = """ SELECT * FROM account_state() """
+    query = f""" SELECT * FROM account_state({curr_day}) """
     cur = conn.cursor()
     cur.execute(query)
     cur.close()
@@ -91,7 +100,7 @@ def account_state():
 
 #Procedimiento 3:
 
-def clients_points_state():
+def clients_points_state(last_date_month):
     query = """ CREATE OR REPLACE FUNCTION client_points_state (last_date_month TIMESTAMP)
     RETURNS TABLE (loyal_client varchar(50), points INTEGER) AS $$ 
     DECLARE 
@@ -117,6 +126,12 @@ def clients_points_state():
     END;
     $$ 
     LANGUAGE plpgsql"""
+    cur = conn.cursor()
+    cur.execute(query)
+    cur.close()
+    conn.commit()
+
+    query = f"""SELECT * FROM clients_points_state({last_date_month})"""
     cur = conn.cursor()
     cur.execute(query)
     cur.close()
